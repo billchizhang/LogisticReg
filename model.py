0bfae31de0688a7aab2d4ccf87d4e169d5c24024
billchizhang <zhangchi6680@gmail.com>
2023-11-04 12:59:09 -0400

    Added Part 2 modeles

--- model.py
+++ model.py
@@ -8,6 +8,8 @@ data = pd.read_csv("HW2_data.csv", usecols=['CHURN', 'tenure', 'monthlycharges',
                                             'DEPENDENTS', 'internetservice', 'FIBER', 'DSL', 'phoneservice'])
 
 df_data = pd.DataFrame(data)
+
+# set up dummy variables
 df_data.loc[df_data["internetservice"] == "DSL", "internetservice"] = 0
 df_data.loc[df_data["internetservice"] == "Fiber optic", "internetservice"] = 1
 df_data.loc[df_data["internetservice"] == "No", "internetservice"] = -1
@@ -15,58 +17,63 @@ df_data.loc[df_data["internetservice"] == "No", "internetservice"] = -1
 df_data.loc[df_data["phoneservice"] == "Yes", "phoneservice"] = 1
 df_data.loc[df_data["phoneservice"] == "No", "phoneservice"] = 0
 
-df_data.loc[df_data["FIBER"] == "Yes", "FIBER"] = 1
-df_data.loc[df_data["FIBER"] == "No", "FIBER"] = 0
 
-df_data.loc[df_data["DSL"] == "Yes", "DSL"] = 1
-df_data.loc[df_data["DSL"] == "No", "DSL"] = 0
+# adding features
+df_data["tenure_half"] = df_data["tenure"] / 2
+df_data["tenure_double"] = df_data["tenure"] * 2
+df_data["tenure_half_DSL"] = df_data["tenure_half"] * df_data["DSL"]
+df_data["tenure_half_FIBER"] = df_data["tenure_half"] * df_data["FIBER"]
+df_data["tenure_double_DSL"] = df_data["tenure_double"] * df_data["DSL"]
+df_data["tenure_double_FIBER"] = df_data["tenure_double"] * df_data["FIBER"]
+
 # split features and labels
-feature_cols = ['tenure', 'monthlycharges', 'PAPERLESSBILLING',
-                                            'DEPENDENTS', 'internetservice', 'FIBER', 'DSL', 'phoneservice']
+''' feature_cols = ['tenure', 'monthlycharges', 'PAPERLESSBILLING',
+                                            'DEPENDENTS', 'internetservice', 'FIBER', 'DSL', 'phoneservice']'''
+feature_cols = ['DSL', 'FIBER', 'tenure', 'monthlycharges']
 X = df_data[feature_cols]
 Y = df_data.CHURN
-
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
-logreg = LogisticRegression(max_iter=200)
-model = logreg.fit(X_train, Y_train)
+logreg = LogisticRegression().fit(X_train, Y_train)
+
 print("Feature Coefs: ")
-print(model.coef_)
+print(logreg.coef_)
 print("Model Intercept")
-print(model.intercept_)
-y_predicted = model.predict(X_test)
+print(logreg.intercept_)
+
+# using training set
+y_predicted = logreg.predict(X_train)
+
+cnf_matrix = metrics.confusion_matrix(Y_train, y_predicted)
+recall = metrics.recall_score(Y_train, y_predicted)
+accuracy = metrics.accuracy_score(Y_train, y_predicted)
+precision = metrics.precision_score(Y_train, y_predicted)
+f1 = metrics.f1_score(Y_train, y_predicted)
+print("Confusion Matrix for training")
+print(cnf_matrix)
+print("Recall for training")
+print(recall)
+print("Accuracy for training")
+print(accuracy)
+print("Precision for training")
+print(precision)
+print("F1 for training")
+print(f1)
+
+y_predicted = logreg.predict(X_test)
+# using testing set
 cnf_matrix = metrics.confusion_matrix(Y_test, y_predicted)
 recall = metrics.recall_score(Y_test, y_predicted)
 accuracy = metrics.accuracy_score(Y_test, y_predicted)
 precision = metrics.precision_score(Y_test, y_predicted)
 f1 = metrics.f1_score(Y_test, y_predicted)
-print("Confusion Matrix for cut-off=0.5")
+print("Confusion Matrix for test")
 print(cnf_matrix)
-print("Recall for cut-off=0.5")
+print("Recall for test")
 print(recall)
-print("Accuracy for cut-off=0.5")
+print("Accuracy for test")
 print(accuracy)
-print("Precision for cut-off=0.5")
+print("Precision for test")
 print(precision)
-print("F1 for training")
+print("F1 for test")
 print(f1)
-
-
-# change the threshold probability to 0.7
-y_predicted_prob = model.predict_proba(X_test)
-y_predicted_class = y_predicted_prob[:, 1]
-y_predicted_updated = [1 if prob >= 0.7 else 0 for prob in y_predicted_class]
-cnf_matrix = metrics.confusion_matrix(Y_test, y_predicted_updated)
-recall = metrics.recall_score(Y_test, y_predicted_updated)
-accuracy = metrics.accuracy_score(Y_test, y_predicted_updated)
-precision = metrics.precision_score(Y_test, y_predicted_updated)
-print("Confusion Matrix for cut-off=0.7")
-print(cnf_matrix)
-print("Recall for cut-off=0.7")
-print(recall)
-print("Accuracy for cut-off=0.7")
-print(accuracy)
-print("Precision for cut-off=0.7")
-print(precision)
-
-
 
